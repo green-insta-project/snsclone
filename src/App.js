105829687a22e957ec994119838dc5bad04f8c10
jihwa <mungsome@gmail.com>
2021-10-26 22:08:09 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,22 @@
-import logo from './logo.svg';
+
+import {HashRouter, Switch, Route, Link} from 'react-router-dom'
+
 import './App.css';
+import Home from './component/Home';
+import Login from './component/Login';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          jihwa
-          yoonseo
-          sohee
-          jooeun
-          jiyeon
-        </a>
-      </header>
-    </div>
+    <HashRouter>
+
+
+      <Switch>
+        <Route exact path="/"><Login /></Route>
+        <Route path="/Home"><Home /></Route>
+      </Switch>
+
+
+    </HashRouter>
   );
 }
 
---
+++ src/component/Login.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Login() {
+  return (
+    <div>
+      <div className="this">hihihi</div>
+
+      로그인하시오^^ -주은님힘내세요-
+    </div>
+  )
+}
