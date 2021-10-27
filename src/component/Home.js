02c573a13785bd39b2a01414918144048797e467
Jiyeon970117 <80682733+Jiyeon970117@users.noreply.github.com>
2021-10-27 16:47:44 +0900

    hi

--- src/component/Home.js
+++ src/component/Home.js
@@ -2,8 +2,29 @@ import React from 'react'
 
 export default function Home() {
   return (
-    <div>
-      홈!^^
-    </div>
+    <main>
+      <section>
+        <article>
+          <div className="header">
+            <div className="left">
+              <figure>
+                <img src/>
+              </figure>
+              <span></span>
+            </div>
+            <div className="right"></div>
+          </div>
+          <div className="content">
+            <img src/>
+          </div>
+          <div>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+        </article>
+      </section>
+    </main>
   )
 }
