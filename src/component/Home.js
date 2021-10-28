24a57638c3790588c4fa0c87ecb282163343b9a0
yyoonseo93 <86402278+yyoonseo93@users.noreply.github.com>
2021-10-28 20:54:20 +0900

    home

--- src/component/Home.js
+++ src/component/Home.js
@@ -1,10 +1,65 @@
 import React from 'react'
 import '../style/Home.scss'
+import story from './img/story.jpg'
+// import { IoIosArrowDropleftCircle } from "react-icons/io";
+// import { IoIosArrowDroprightCircle } from "react-icons/io";
 
 export default function Home() {
   return (
     <div className="this">
-      홈!^^
+      <div className="story">
+        <div className="story-contents">
+          <ul>
+            <li>
+              <div>
+                <img src={story}/>
+              </div>
+              <p>yoons_93</p>
+            </li>
+            <li>
+              <div>
+                <img src={story}/>
+              </div>
+              <p>yoons_93</p>
+            </li>
+            <li>
+              <div>
+                <img src={story}/>
+              </div>
+              <p>yoons_93</p>
+            </li>
+            <li>
+              <div>
+                <img src={story}/>
+              </div>
+              <p>yoons_93</p>
+            </li>
+            <li>
+              <div>
+                <img src={story}/>
+              </div>
+              <p>yoons_93</p>
+            </li>
+            <li>
+              <div>
+                <img src={story}/>
+              </div>
+              <p>yoons_93</p>
+            </li>
+            <li>
+              <div>
+                <img src={story}/>
+              </div>
+              <p>yoons_93</p>
+            </li>
+          </ul>
+        </div>
+        <div className="story-arrow">
+          {/* <button className="story-leftarrow"><IoIosArrowDropleftCircle size="1.2em"/></button>
+          <button className="story-rightarrow"><IoIosArrowDroprightCircle size="1.2em"/></button> */}
+        </div>
+        
+      </div>
     </div>
   )
 }
